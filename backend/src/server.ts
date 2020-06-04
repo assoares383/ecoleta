a013b05690099273aeb2de6a0464c0be88e10a57
assoares383 <alexandressoares383@gmail.com>
2020-06-03 21:29:11 -0300

    backend project create

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  response.json("Hello World!");
+});
+
+app.listen(3333, () => {
+  console.log("Server running...");
+});
