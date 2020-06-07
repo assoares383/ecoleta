ed4a35a41cf5b74353a811e6cd33d6555e9cb993
assoares383 <alexandressoares383@gmail.com>
2020-06-07 20:30:02 -0300

    carregar a localização atual do usuário no mapa

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -29,12 +29,25 @@ const CreatePoint = () => {
   const [ufs, setUfs] = useState<string[]>([]);
   const [cities, setCities] = useState<string[]>([]);
 
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([
+    0,
+    0,
+  ]);
+
   const [selectedUf, setSelectedUf] = useState("0");
   const [selectedCity, setSelectedCity] = useState("0");
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([
     0,
     0,
   ]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+
+      setInitialPosition([latitude, longitude]);
+    });
+  }, []);
 
   useEffect(() => {
     api.get("items").then((response) => {
@@ -124,11 +137,7 @@ const CreatePoint = () => {
             <span>Selecione o endereço do mapa</span>
           </legend>
 
-          <Map
-            center={[-27.2092052, -49.6401092]}
-            zoom={15}
-            onClick={handleMapClick}
-          >
+          <Map center={initialPosition} zoom={17} onClick={handleMapClick}>
             <TileLayer
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
